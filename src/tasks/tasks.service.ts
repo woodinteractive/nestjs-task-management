6428bd813e39bbed9ccc9c3c97ae33d3fa8dd61d
Daniel Wood <danielwood@Daniels-MBP.attlocal.net>
2019-12-08 22:30:50 -0600

    added validation handling for task controller methods

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import * as uuid from 'uuid/v1';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -28,6 +28,9 @@ export class TasksService {
 
   getTaskById(id: string): Task {
     const found = this.tasks.find(task => task.id === id)
+    if (!found) {
+      throw new NotFoundException()
+    }
     return found
   }
 
@@ -45,7 +48,8 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter(task => task.id !== id)
+    const found = this.getTaskById(id)
+    this.tasks = this.tasks.filter(task => task.id !== found.id)
   }
 
   updateTaskStatus(id: string, status: TaskStatus): Task {
